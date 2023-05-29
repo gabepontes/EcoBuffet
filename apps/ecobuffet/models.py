7f7635be7ef2d4268c417a82b4b867998f2fa79d
Raymond Huang <rhuang43@ucsc.edu>
2023-05-28 23:28:17 -0700

    added menu page dark mode but will fix it tmr

--- apps/ecobuffet/models.py
+++ apps/ecobuffet/models.py
@@ -101,6 +101,6 @@ def add_restaurants_for_testing(num_restaurants, num_items_per_restaurant):
 
     db.commit()
 
-# Create 5 restaurants each with 10 items
-#add_restaurants_for_testing(5, 10)
+# # # Create 5 restaurants each with 10 items
+# add_restaurants_for_testing(5, 10)
 
